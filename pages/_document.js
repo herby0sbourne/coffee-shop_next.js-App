aefae3939b019dca547a8edc03cab8b0caa7495a
Herby <herbyosbourne31@gmail.com>
2022-12-19 21:23:02 -0500

    ad font to head tag

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,32 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link
+          rel="perload"
+          href="/fonts/Poppins-Bold.ttf"
+          as="font"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="perload"
+          href="/fonts/Poppins-Regular.ttf"
+          as="font"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="perload"
+          href="/fonts/Poppins-SemiBold.ttf"
+          as="font"
+          crossOrigin="anonymous"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
