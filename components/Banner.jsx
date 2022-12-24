f3a22b124b1b51281cce7a045e42ff6a31eff4b3
Herby <herbyosbourne31@gmail.com>
2022-12-24 09:34:18 -0500

    create banner component

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -1,10 +1,11 @@
-import styles from './banner.module.css';
+import styles from "./banner.module.css";
+import cls from "classnames";
 
 const Banner = ({ buttonText, handleOnClick }) => {
   return (
-    <div className={styles.container}>
+    <div className={cls("bnn", styles.container)}>
       <h1 className={styles.title}>
-        <span className={styles.title1}>Coffee</span>{' '}
+        <span className={styles.title1}>Coffee</span>{" "}
         <span className={styles.title2}>Connoisseur</span>
       </h1>
       <p className={styles.subtitle}>Discover your local coffee shops!</p>
