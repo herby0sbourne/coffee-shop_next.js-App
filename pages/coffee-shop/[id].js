a63cd6fa3eeda902d41f8c8c68d185dffad88fe1
Herby <herbyosbourne31@gmail.com>
2022-12-24 09:44:48 -0500

    create dynamic route for coffee store

--- pages/coffee-shop/[id].js
+++ pages/coffee-shop/[id].js
@@ -1,16 +1,126 @@
+import { useContext, useEffect, useState } from "react";
 import { useRouter } from "next/router";
+import Head from "next/head";
 import Link from "next/link";
+import Image from "next/image";
+import cls from "classnames";
 
-const Id = () => {
+import { fetchCoffeeStores } from "../../lib/fetchCoffeeStores";
+import { isEmpty } from "../../utils/isEmpty";
+import { StoreContext } from "../../store/storeContext";
+
+import nearMe from "../../public/static/icons/nearMe.svg";
+import places from "../../public/static/icons/places.svg";
+import star from "../../public/static/icons/star.svg";
+import styles from "../../styles/coffee-shop.module.css";
+
+export async function getStaticProps(staticProps) {
+  const params = staticProps.params;
+  // "40.7420277,-74.0684579"
+  const coffeeStores = await fetchCoffeeStores();
+  const coffeeStore = coffeeStores.find((coffeeStore) => {
+    return coffeeStore.id.toString() === params.id;
+  });
+
+  return {
+    props: {
+      coffeeStore: coffeeStore ? coffeeStore : {},
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  const coffeeStores = await fetchCoffeeStores();
+
+  const paths = coffeeStores.map((coffeeStore) => {
+    return { params: { id: coffeeStore.id.toString() } };
+  });
+
+  return {
+    paths,
+    fallback: true,
+  };
+}
+
+const CoffeeStore = (initialProps) => {
+  const { state } = useContext(StoreContext);
+  const [coffeeStore, setCoffeeStore] = useState(initialProps.coffeeStore);
   const router = useRouter();
-  console.log(router);
+
+  const id = router.query.id;
+
+  useEffect(() => {
+    if (!isEmpty(initialProps.coffeeStore)) return;
+
+    if (!state.coffeeStores.length) return;
+
+    const coffeeStore = state.coffeeStores.find((coffeeStore) => {
+      return coffeeStore.id.toString() === id;
+    });
+    setCoffeeStore(coffeeStore);
+
+    // if (isEmpty(initialProps.coffeeStore)) {
+    //   if (state.coffeeStores.length > 0) {
+    //     const coffeeStore = state.coffeeStores.find((coffeeStore) => {
+    //       return coffeeStore.id.toString() === id;
+    //     });
+    //     setCoffeeStore(coffeeStore);
+    //   }
+    // }
+  }, [id]);
+
+  if (router.isFallback) return <div>Loading...</div>;
+  const { name, address, neighborhood, imgUrl } = coffeeStore;
+
+  const handleUpvoteButton = () => {
+    console.log("upvote");
+  };
+
   return (
-    <h1>
-      coffee shopp {router.query.id}
-      <Link href="/">Back to Home</Link>
-      <Link href={`/coffee-shop/${router.query.id}`}>dynamic route</Link>
-    </h1>
+    <div className={styles.layout}>
+      <Head>
+        <title>{name}</title>
+      </Head>
+      <div className={styles.container}>
+        <div className={styles.col1}>
+          <h1>coffee shop</h1>
+          <div className={styles.backToHomeLink}>
+            <Link href="/">&larr; Back to Home</Link>
+          </div>
+          <div className={styles.nameWrapper}>
+            <h1 className={styles.name}>{name}</h1>
+          </div>
+          <Image
+            src={
+              imgUrl ||
+              "https://imageio.forbes.com/specials-images/imageserve/60f5b60d124afbc1596f1489/0x0.jpg?format=jpg&width=1200"
+            }
+            alt={`${name} image`}
+            width={600}
+            height={360}
+            className={styles.storeImg}
+          />
+        </div>
+        <div className={cls("glass", styles.col2)}>
+          <div className={styles.iconWrapper}>
+            <Image src={places} width={25} height={25} alt="location icon" />
+            <p className={styles.text}>{address}</p>
+          </div>
+          <div className={styles.iconWrapper}>
+            <Image src={nearMe} width={25} height={25} alt="location icon" />
+            <p className={styles.text}>{neighborhood}</p>
+          </div>
+          <div className={styles.iconWrapper}>
+            <Image src={star} width={25} height={25} alt="location icon" />
+            <p className={styles.text}>1</p>
+          </div>
+          <button className={styles.upvoteButton} onClick={handleUpvoteButton}>
+            Up vote!
+          </button>
+        </div>
+      </div>
+    </div>
   );
 };
 
-export default Id;
+export default CoffeeStore;
