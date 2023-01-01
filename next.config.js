b9d4f54453d00f73c0003227b69b9eadf950a0c5
Herby <herbyosbourne31@gmail.com>
2023-01-01 07:25:37 -0500

    update config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  // env: {},
+  images: {
+    domains: [
+      "images.unsplash.com",
+      "media.gettyimages.com",
+      "imageio.forbes.com",
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
